7674bf5d4c17d6c26f842b029b7873c31d133d73
Karamjeet Singh <janduk@student.douglascollege.ca>
2022-09-01 23:39:41 -0700

    crash course done

---
+++ main.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, Path, Query, HTTPException, status
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
+
+@app.get("/")
+def home():
+    return {"Data": "Testing"}
+
+@app.get("/about")
+def about():
+    return {"Data": "About"}
+
+inventory = {}
+
+@app.get("/get-items")
+def get_items():
+    return inventory
+
+@app.get("/get-item/{item_id}}")
+def get_item(item_id: int = Path(None ,description="The ID of the item you would like to view", gt=0)):
+    return inventory.get(item_id)
+
+@app.get("/get-by-name/{item_id}")
+def get_item(*, item_id: int, name: Optional[str] = None, test: int):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+
+    raise HTTPException(status_code=404, detail="Item ID not found.")
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        raise HTTPException(status_code=400, detail="Item ID already exists.")
+    
+    inventory[item_id] = item
+
+    return inventory[item_id]
+
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
+    if item_id not in inventory:
+        raise HTTPException(status_code=400, detail="Item ID does not exists.")
+     
+    if item.name:
+        inventory[item_id].name = item.name 
+
+    if item.price:
+        inventory[item_id].price = item.price 
+
+    if item.brand:
+        inventory[item_id].brand = item.brand 
+
+    return inventory[item_id]
+
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query(..., description="The id of item to delete", gt=0)):
+    if item_id not in inventory:
+        return {"Error": "Item does not exists"}
+
+    del inventory[item_id]
+
+    return {"Success": "Item deleted"}
+    
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, reload=True, log_level="info")
